10d056e9727c95ef066767729ae61d46570bc27d
Harshad Dange <hasse@l1301.core.reaktor.fi>
2018-12-20 13:25:17 +0100

    Added basic HTML file translation

--- ducky-api/app.js
+++ ducky-api/app.js
@@ -9,6 +9,7 @@ const path = require('path');
 const fs = require('fs');
 const exec = require('child_process').exec;
 const rimraf = require('rimraf');
+const htmlParser = require('htmlParser2');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
@@ -62,7 +63,7 @@ app.post('/translate/image', upload.single('image'), function(req, res) {
 		}
 		performOCR(targetPath)
 		.then( text => {
-			translateText(text)
+			translateArray(text)
 			.then( result => {
 				postSuccess(res, result);
 			})
@@ -76,9 +77,103 @@ app.post('/translate/image', upload.single('image'), function(req, res) {
 	});
 });
 
+app.post('/translate/html', upload.single('html'), function(req, res) {
+	const tempPath = req.file.path;
+	const name = shortid.generate() + '.' + (path.extname(req.file.originalname).extname || 'html');
+	const targetPath = path.join(__dirname, `./uploads/${name}`);
+	fs.rename(tempPath, targetPath, err => {
+		if (err) {
+			postError(res, 500, 'There was an error');
+			return;
+		}
+		translateHTMLFile(targetPath)
+		.then(result => {
+			postSuccess(res, { html: result });
+		})
+		.catch(err => {
+			postError(res, 500, 'Could not translate');
+		});
+	});
+});
+
 /**
  * Helpers
  */
+
+function translateHTMLFile(path) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(path, { encoding: 'utf8' }, (err, html) => {
+			if (err) { reject('Cannot read file'); return; }
+
+			const ignoredTags = ['script', 'head', 'style', 'noscript', 'meta', 'html'];
+			let ignoreNext = false;
+			let toTranslate = [];
+
+			const parser = new htmlParser.Parser({
+				onopentag: (name, attribute) => {
+					if (ignoredTags.find( t => { return t === name.trim().toLowerCase() })) {
+						ignoreNext = true;
+					}
+				},
+				ontext: (text) => {
+					if(!ignoreNext) {
+						toTranslate.push(text);
+					}
+				},
+				onclosetag: (name, attribute) => {
+					ignoreNext = false;
+				}
+			});
+			parser.write(html);
+			parser.end();
+
+			translateArray(toTranslate)
+			.then(result => {
+				if (result.length !== toTranslate.length) { reject('Translation failed'); return; }
+				let translated = '';
+				let index = 0;
+
+				const writer = new htmlParser.Parser({
+					onopentag: (name, attributes) => {
+						translated = translated + `\n<${name} ${stringifyAttributes(attributes)}>`;
+						if (ignoredTags.find( t => { return t === name.trim().toLowerCase() })) {
+							ignoreNext = true;
+						}
+					},
+					ontext: (text) => {
+						if(ignoreNext) {
+							translated = translated + text;
+						} else {
+							translated = translated + result[index].translation;
+							index++;
+						}
+					},
+					onclosetag: (name, attribute) => {
+						ignoreNext = false;
+						translated = translated + `</${name}>`
+					}
+				});
+				writer.write(html);
+				writer.end();
+				resolve(translated);
+			})
+			.catch(err => {
+				reject(err);
+			});
+		});
+	});
+}
+
+function stringifyAttributes(attributes) {
+	if (!attributes) { return ' '; }
+	let strung = '';
+	for (const name in attributes) {
+		if (attributes.hasOwnProperty(name)) {
+			strung += ` ${name}="${attributes[name]}"`
+		}
+	}
+	return strung;
+}
 
 function performOCR(fileName) {
 	return new Promise(function(resolve, reject) {
@@ -128,6 +223,40 @@ function translateText(text) {
 				return;
 			}
 			resolve(response.data.translations[0]);
+		})
+		.catch(error => {
+			reject("Failed to translate");
+		});
+	});
+}
+
+function translateArray(texts) {
+	return new Promise(function(resolve, reject) {
+		const watsonKey = config.watsonKey;
+		const watsonURL = config.watsonURL;
+		if (!watsonKey || !watsonURL) {
+			reject('We cannot proceed at this moment');
+			return;
+		}
+		if (!texts) {
+			reject('Nothing to translate');
+			return;
+		}
+
+		var reqConfig = { auth: { username: 'apiKey', password:  watsonKey }};
+
+		axios.post(watsonURL, {
+			text: texts,
+			'model-id': 'nl-en',
+			'source': 'nl',
+			'target': 'en'
+		}, reqConfig)
+		.then((response) => {
+			if (!response.data.translations || response.data.translations.length == 0) {
+				reject("Failed to translate");
+				return;
+			}
+			resolve(response.data.translations);
 		})
 		.catch(error => {
 			reject("Failed to translate");
